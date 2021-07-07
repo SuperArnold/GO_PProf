5ac51609dc03c046e3fd8e19d2e1a037c5c08991
SuperArnold <ji394vul35520@hotmail.com>
2021-07-08 00:44:10 +0800

    修改為Mutex Profiling

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	_ "net/http/pprof"
-	"time"
+	"runtime"
+	"sync"
 )
 
 var datas []string
 
+func init() {
+	runtime.SetMutexProfileFraction(1)
+}
 func main() {
-	go func() {
-		for {
-			log.Printf("len: %d", Add("GO PProf demo"))
-			time.Sleep(time.Millisecond * 10)
-		}
-	}()
-	_ = http.ListenAndServe("0.0.0.0:6060", nil)
+	var m sync.Mutex
+	var datas = make(map[int]struct{})
+	for i := 0; i < 999; i++ {
+		go func(i int) {
+			m.Lock()
+			defer m.Unlock()
+			datas[i] = struct{}{}
+		}(i)
+	}
+	_ = http.ListenAndServe("0.0.0.0:6061", nil)
 
 }
 
